402ff0948e24fd2026b3ef5f4b3580bacb02698a
denisesh <denisesh@gmail.com>
2014-12-31 11:43:09 -0800

    logic for game works

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,10 @@
 class Game
-  attr_accessor :input, :output, :choices, :choice
+  attr_accessor :input, :output, :choices
 
   def initialize (input, output)
     @input = input
     @output = output
     @choices = ["rock", "paper", "scissors"]
-    @choice = choice
   end
 
   def run
@@ -13,59 +12,53 @@ class Game
     Welcome to the game
     """
     quit = false
-
+    p quit
     unless quit
       puts "still playing"
       get_choices_user
       get_choices_computer
-      # winner = determine_winner(choice, computer_choice)
-      # puts "...and the winner is #{winner}"
-      #find_winner
-      #quit_the_game?
+      determine_winner(@choice, @computer_choice)
+      quit_game
     end
-
-    def quit_the_game?
-      puts "Do you want to play again?"
-      response = gets.chomp
-      if response
-        quit = false
-      else
-        quit = true
-      end
-    end
-
   end
 
   def get_choices_user
     puts "What sign would you like to throw?"
     puts "Your choices are paper, rock, scissors "
-    choice = gets.chomp
-    unless @choices.include?(choice)
+    @choice = gets.chomp
+    unless @choices.include?(@choice)
       puts "sorry only paper, rock, scissors if you wanna play"
-      choice = gets.chomp
+      @choice = gets.chomp
     end
-    choice
+    @choice
   end
 
   def get_choices_computer
-    computer_choice = choices.sample
-    puts "You chose #{choice}"
-    puts "Your opponent chose #{computer_choice} "
+    @computer_choice = choices.sample
+    puts "You chose #{@choice}"
+    puts "Your opponent chose #{@computer_choice} "
   end
 
   def determine_winner(choice, computer_choice)
-    if choice == computer_choice
-      puts "you tied"
-    elsif choice == winning_choice(computer_choice)
-      choice
+    convert = {"rock"=> 0, "paper"=> 1,"scissors"=> 2,}
+    winner = (3 + convert[choice] - convert[computer_choice]) % 3
+      if winner == 1
+        puts "you beat the computer!"
+      elsif winner == 2
+        puts "dang, you lost..."
+      else
+        puts "Its a tie!"
+      end
+  end
+
+  def quit_game
+    puts "Do you want to play again?"
+    response = gets.chomp
+    if response
+      quit = false
     else
-      computer_choice
+      quit = true
     end
   end
-
-  def winning_choice(choice)
-    #games = determine winning choice
-  end
-
 
 end
